b4503e16983937f457159f34ecdca345a2663b64
Malaika Riasat <malaika.202404824@gmail.com>
2026-06-28 12:14:24 +0500

    commit

--- 03_coseWChai.js
+++ 03_coseWChai.js
@@ -1,1 +1,6 @@
-console.log("malaika");
+const user_id = 1212; //canot change value in futer
+let name="MALAIKA";//change value any time
+fatherName="Riasat";// js provide the facelity that we assign variable without any data type.==> but the is not good practics
+
+console.log(name); //single output 
+console.table([user_id,name,fatherName]) // all variable show in form of table at same time
