90e241a22ac0209fce37921dccb52d52c201e2fb
Malaika Riasat <malaika.202404824@gmail.com>
2026-07-04 14:57:04 +0500

    commit

---
+++ Day_1.js
@@ -0,0 +1,13 @@
+console.log("hellow");
+console.log(2+4);
+if (age=12){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+ // Recursion 
+//  when function calls itself to slove a problem
+function car(){
+    modal = hunda
+}
