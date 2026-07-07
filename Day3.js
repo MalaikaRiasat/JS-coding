3a90ee0e9046a25ba1ac1c0e6eb9250a8a2cfe2a
Malaika Riasat <malaika.202404824@gcuf.edu.pk>
2026-07-07 23:36:13 +0500

    Day3 javaScript

---
+++ Day3.js
@@ -0,0 +1,36 @@
+//loops
+for(let i=1;i<=10;i++){
+    console.log("i=",i);
+}
+
+// while loop
+let x=1;
+while(x<=10){
+    console.log("x=",x);
+    x++;
+}
+
+//do while loop
+let z=1;
+do{
+    console.log("z=",z);
+    z++;
+}while(z<=10);
+
+
+//for-of loop   use with string and array
+let name="Malai"
+for(let y of name){
+    console.log("y=",y);
+
+};
+
+//for-in loop    use with object
+let student={
+    name:"Malaika",
+    age:30
+};
+
+for (let s in student){
+    console.log("s=",s);  // only print there key names
+}
