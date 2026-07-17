7421ab39d463282b1cc65f8219c9cdf8729d5ba8
Malaika Riasat <malaika.202404824@gcuf.edu.pk>
2026-07-18 00:05:57 +0500

    comit

--- Day4.js
+++ Day4.js
@@ -3,12 +3,16 @@
 let marks=[12,28,28,18,19];
 console.log(marks);
 console.log(marks[1]);
-console.log(marks[4]);
+console.log(marks.length);
+console.log(typeof(marks));  //objects
+
 
 console.log("Malaika's marks:",marks[2]);
 
 
 console.log("sana's marks:",marks[1]);
 
+marks[0]=23;      // arrys in javascript are mutable. 
+console.log("Malaika's marks:",marks[0]);
 
 //concept number 2
